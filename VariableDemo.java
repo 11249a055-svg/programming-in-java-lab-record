32feb54bf6b572fddd5720aa1bb9f8a7ad853f1c
11249a055-svg <11249a055@kanchiuniv.ac.in>
2026-07-22 11:58:09 +0530

    Add files via upload

---
+++ VariableDemo.java
@@ -0,0 +1,18 @@
+public class VariableDemo{
+int instanceVar = 10;
+static String staticVar = "I am static";
+public void showVariables()
+{
+int localVar = 5;
+System.out.println("Instance Variable:"+instanceVar);
+System.out.println("Static Variable:"+staticVar);
+System.out.println("Local Variable:"+localVar);
+}
+public static void main(String[]args)
+{
+VariableDemo obj1 = new VariableDemo();
+obj1.showVariables();
+System.out.println(
+"Accessing Static Variable via class:"+VariableDemo.staticVar);
+}
+}
