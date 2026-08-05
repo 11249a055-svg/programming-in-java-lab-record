72c3a712b1ebfc7df5699c53906dc82efc8708c6
11249a055-svg <11249a055@kanchiuniv.ac.in>
2026-08-05 11:38:39 +0530

    Add files via upload

---
+++ StaticDemo.java
@@ -0,0 +1,8 @@
+public  class StaticDemo{
+    static void display(){
+        System.out.println("this is a static method");
+    }
+    public static void main(String[] args){
+        StaticDemo.display();
+    }
+}
