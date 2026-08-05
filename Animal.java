35ddba8564944484ec4b8d3c86da40da8a5c145a
11249a055-svg <11249a055@kanchiuniv.ac.in>
2026-08-05 11:39:41 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    final void sound() {
+        System.out.println("Animals make sounds.");
+    }   
+}
+class FinalMethodDemo {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.sound();
+    }    
+}
