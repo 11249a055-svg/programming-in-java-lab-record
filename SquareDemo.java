66167adc60dafafe82106635eed90fedc811fb3a
11249a055-svg <11249a055@kanchiuniv.ac.in>
2026-08-05 11:39:01 +0530

    Add files via upload

---
+++ SquareDemo.java
@@ -0,0 +1,8 @@
+public class SquareDemo {
+    static void square(int n){
+        System.out.println("Square of "+ n +" =" +(n*n));
+}
+public static void main(String[] args) {
+    SquareDemo.square(7);
+}
+}
