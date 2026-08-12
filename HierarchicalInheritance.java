334600230e9ea499b40aacb8d9f0f6d8ac75fa38
11249a055-svg <11249a055@kanchiuniv.ac.in>
2026-08-12 11:27:56 +0530

    Add files via upload

---
+++ HierarchicalInheritance.java
@@ -0,0 +1,26 @@
+class Animal{
+    void eat() {
+        System.out.println("Animal is  eating");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("dog is barking");
+    }
+}
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Cat is meowing");
+    }
+}
+public class HierarchicalInheritance{
+    public static void main (String[] args){
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+        Cat c = new Cat();
+        c.eat();
+        c.meow();
+    }
+}                
+       
